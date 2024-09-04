2b6b6d40aeb4a3cdd0743f54d8628b945a960612
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-09-04 14:25:49 -0500

    uncaughtException implemented

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,21 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/errors.js";
 import { notFound } from "./utils/errorHandler.js";
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught expection");
+  process.exit(1);
+});
+
 // database connection
 connectDatabase();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+// console.log(first) // to test uncaughtException
+
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
@@ -25,8 +35,19 @@ app.use(errorMiddleware);
 // Handle not found
 app.use(notFound);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   console.log(
-    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+    `Server started on PORT: ${process.env.PORT || 5000} in ${
+      process.env.NODE_ENV || "test"
+    } mode.`
   );
+});
+
+//Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
