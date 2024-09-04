5afb186885c0fe33034ebadb7c4637f48fe2ffee
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-09-04 13:13:58 -0500

    deleteProductHandler implementation initialized

--- backend/handlers/productHandlers.js
+++ backend/handlers/productHandlers.js
@@ -73,3 +73,25 @@ export const updateProductHandler = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+// Delete product   =>  /api/v1/admin/products/:id
+export const deleteProductHandler = async (req, res) => {
+  try {
+    const product = await Product.findById(req?.params?.id);
+
+    if (!product) {
+      return res.status(404).json({
+        // success: false,
+        error: "Product not found",
+      });
+    }
+
+    await product.deleteOne();
+    res.status(200).json({
+      // success: true,
+      message: "Product is deleted",
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,6 +4,7 @@ import {
   newProductHandler,
   getProductDetailsHandler,
   updateProductHandler,
+  deleteProductHandler,
 } from "../handlers/productHandlers.js";
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.route("/products").get(getProductsHandler);
 router.route("/admin/products").post(newProductHandler);
 router.route("/products/:id").get(getProductDetailsHandler);
 router.route("/products/:id").put(updateProductHandler);
+router.route("/products/:id").delete(deleteProductHandler);
 
 export default router;
