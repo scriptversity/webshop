b35c33511f6884d1a074cd8f1e0cb4e0354da8e1
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-09-04 13:00:32 -0500

    getProductDetailsHandler implementation initialized

--- backend/handlers/productHandlers.js
+++ backend/handlers/productHandlers.js
@@ -8,7 +8,7 @@ export const getProductsHandler = async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
-}
+};
 
 // Create new product => /api/v1/admin/products
 export const newProductHandler = async (req, res) => {
@@ -16,9 +16,30 @@ export const newProductHandler = async (req, res) => {
     const product = await Product.create(req.body);
     res.status(201).json({
       success: true,
-      product
+      product,
     });
   } catch (error) {
     res.status(500).json(error);
   }
-}
+};
+
+// Get single product details   =>  /api/v1/products/:id
+export const getProductDetailsHandler = async (req, res) => {
+  try {
+    const product = await Product.findById(req?.params?.id);
+
+    if (!product) {
+      return res.status(404).json({
+        // success: false,
+        error: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   getProductsHandler,
-  newProductHandler
+  newProductHandler,
+  getProductDetailsHandler
 } from "../handlers/productHandlers.js";
 
 const router = express.Router();
 
-router.route("/admin/products").post(newProductHandler);
 router.route("/products").get(getProductsHandler);
+router.route("/admin/products").post(newProductHandler);
+router.route("/products/:id").get(getProductDetailsHandler);
 
 export default router;
