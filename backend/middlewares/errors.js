2959465e4abe157f18b1489d6402b28e845268f1
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-09-05 15:44:44 -0500

    Handle Mongoose duplicate key error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -24,6 +24,12 @@ export const errorMiddleware = function (err, req, res, next) {
     error = new ErrorHandler(message, 400);
   }
 
+  // Handle Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    error = new ErrorHandler(message, 400);
+  }
+
   if (process.env.NODE_ENV === "DEVELOPMENT") {
     res.status(error.statusCode).json({
       message: error.message,
