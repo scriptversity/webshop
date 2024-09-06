738da7a00444283039bb5c35b6d8d8aa2a7965b1
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-09-05 20:11:14 -0500

    updateOrderHandler and allOrdersHandler coded

--- backend/handlers/orderHandlers.js
+++ backend/handlers/orderHandlers.js
@@ -4,8 +4,7 @@ import Order from "../models/order.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 // Create new Order  =>  /api/v1/orders/new
-export const newOrderHandler  = catchAsyncErrors(async (req, res, next) => {
-
+export const newOrderHandler = catchAsyncErrors(async (req, res, next) => {
   const {
     orderItems,
     shippingInfo,
@@ -34,7 +33,7 @@ export const newOrderHandler  = catchAsyncErrors(async (req, res, next) => {
     // success: true,
     order,
   });
-})
+});
 
 // Get current logged in user orders =>  /api/v1/me/orders
 export const myOrdersHandler = catchAsyncErrors(async (req, res, next) => {
@@ -43,19 +42,78 @@ export const myOrdersHandler = catchAsyncErrors(async (req, res, next) => {
     // success: true,
     orders,
   });
-})
+});
 
 // Get order details =>  /api/v1/orders/:id
-export const getOrderDetailsHandler = catchAsyncErrors(async (req, res, next) => {
-  const order = await Order.findById(req.params.id).populate(
-    "user",
-    "name email"
-  );
-  if (!order) {
-    return next(new ErrorHandler("Order not found with this ID" + req.params.id, 404));
+export const getOrderDetailsHandler = catchAsyncErrors(
+  async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+    if (!order) {
+      return next(
+        new ErrorHandler("Order not found with this ID" + req.params.id, 404)
+      );
+    }
+    res.status(200).json({
+      // success: true,
+      order,
+    });
   }
+);
+
+// Get all orders - Admin =>  /api/v1/admin/orders
+export const allOrdersHandler = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+  // const orders = await Order.find({});
+
+  res.status(200).json({
+    // success: true,
+    orders,
+  });
+});
+
+// Update / Process order - Admin =>  /api/v1/admin/orders/:id
+export const updateOrderHandler = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ErrorHandler("Order not found with this ID" + req.params.id, 404)
+    );
+  }
+
+  if (order?.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order?.orderItems?.forEach(async (item) => {
+    const product = await Product.findById(item?.product?.toString());
+
+    if (!product) {
+      return next(
+        new ErrorHandler(
+          "Product not found with this ID" + item?.product?.toString(),
+          404
+        )
+      );
+    }
+
+    product.stock = product?.stock - item?.quantity;
+    // await product?.save();
+    await product?.save({ validateBeforeSave: false });
+  });
+
+  order.orderStatus = req.body.status;
+  // if (req.body.status === "Delivered") {
+  //   order.deliveredAt = Date.now();
+  // }
+  order.deliveredAt = Date.now();
+
+  await order.save();
+  // await order.save({ validateBeforeSave: false });
   res.status(200).json({
     // success: true,
     order,
   });
-})
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,6 +3,8 @@ import {
   newOrderHandler,
   getOrderDetailsHandler,
   myOrdersHandler,
+  allOrdersHandler,
+  updateOrderHandler,
 } from "../handlers/orderHandlers.js";
 
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
@@ -12,5 +14,13 @@ const router = express.Router();
 router.route("/orders/new").post(isAuthenticatedUser, newOrderHandler);
 router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetailsHandler);
 router.route("/me/orders").get(isAuthenticatedUser, myOrdersHandler);
+
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrdersHandler);
+
+router
+  .route("/admin/orders/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrderHandler);
 
 export default router;
